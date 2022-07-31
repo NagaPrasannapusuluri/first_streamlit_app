70a54ce2d20d19a049e1d0e569d5f6bd771cc3a7
NagaPrasannapusuluri <109733292+NagaPrasannapusuluri@users.noreply.github.com>
2022-07-31 15:54:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,19 @@ import streamlit
 streamlit.title("My parents new healthy diner")
 
 
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+streamlit.dataframe(my_fruit_list)
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
