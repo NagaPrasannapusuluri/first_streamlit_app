976e29900bfd2821bc7112acee09e40a4572f3af
NagaPrasannapusuluri <109733292+NagaPrasannapusuluri@users.noreply.github.com>
2022-07-31 19:02:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,11 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+
+streamlit.title("My mom new healthy diner")
+streamlit.header('Breakfast Menu')
+streamlit.text(🥣'Omega 3 & Blueberry Oatmeal')
+streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
+streamlit.text(🐔'Hard-Boiled Free-Range Egg')
+streamlit.text(🥑🍞,'Avocado toast')
